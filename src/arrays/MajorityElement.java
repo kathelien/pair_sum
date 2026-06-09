9b31c0837cd4968f22da95766e8f3a4fe592a52e
UnGitHubUsername <UnGitHubEmail@gmail.com>
2026-06-09 21:07:51 +0530

    Added Majority Element program

---
+++ src/arrays/MajorityElement.java
@@ -0,0 +1,45 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MajorityElement
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        int majority = arr[0];
+        int count = 1;
+
+        for(int i = 1; i < n; i++)
+        {
+            if(arr[i] == majority)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+
+            if(count == 0)
+            {
+                majority = arr[i];
+                count = 1;
+            }
+        }
+
+        System.out.println("Majority Element = " + majority);
+    }
+}
